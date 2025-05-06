6b52033121767f5393c76cb2cfc75c1592ce99ce
NarShaddaa <radius-unos@mail.ru>
2025-05-06 17:32:15 +0300

    +hmpage

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'catalog.apps.CatalogConfig',
+    'homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
